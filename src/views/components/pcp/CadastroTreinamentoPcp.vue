c8c1f0ddf944872456b87e2d82312ddb1414b20c
Hendrius Félix <hendriusfelix@gmail.com>
2024-09-06 17:05:07 -0300

    inicio de aplicação para cadastro de treinamentos PCP

---
+++ src/views/components/pcp/CadastroTreinamentoPcp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h4>Cadastro de Treinamentos</h4>
+    {{ test }}
+  </div>
+</template>
+
+<script>
+// import axios from "axios"
+
+export default {
+  name: "CadastroTreinamentoPcp",
+  components: {},
+
+  data() {
+    return {
+      test: "teste",
+    };
+  },
+};
+</script>
+
+<style></style>
